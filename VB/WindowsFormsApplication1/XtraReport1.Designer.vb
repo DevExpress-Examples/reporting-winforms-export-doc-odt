f79a859bef2878d44f480aae368182799d8cef39
DevExpressExampleBot <fakeEmail>
2019-10-03 14:39:14 +0300

    Source auto update [skip ci]

---
+++ VB/WindowsFormsApplication1/XtraReport1.Designer.vb
@@ -0,0 +1,356 @@
+﻿Namespace WindowsFormsApplication1
+	Partial Public Class XtraReport1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.components = New System.ComponentModel.Container()
+			Dim selectQuery1 As New DevExpress.DataAccess.Sql.SelectQuery()
+			Dim column1 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression1 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim table1 As New DevExpress.DataAccess.Sql.Table()
+			Dim column2 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression2 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column3 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression3 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column4 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression4 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column5 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression5 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column6 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression6 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim selectQuery2 As New DevExpress.DataAccess.Sql.SelectQuery()
+			Dim column7 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression7 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim table2 As New DevExpress.DataAccess.Sql.Table()
+			Dim column8 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression8 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column9 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression9 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column10 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression10 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column11 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression11 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column12 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression12 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column13 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression13 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column14 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression14 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column15 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression15 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column16 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression16 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column17 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression17 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim masterDetailInfo1 As New DevExpress.DataAccess.Sql.MasterDetailInfo()
+			Dim relationColumnInfo1 As New DevExpress.DataAccess.Sql.RelationColumnInfo()
+			Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(XtraReport1))
+			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+			Me.xrLabel1 = New DevExpress.XtraReports.UI.XRLabel()
+			Me.xrPictureBox1 = New DevExpress.XtraReports.UI.XRPictureBox()
+			Me.xrTable1 = New DevExpress.XtraReports.UI.XRTable()
+			Me.xrTableRow1 = New DevExpress.XtraReports.UI.XRTableRow()
+			Me.xrTableCell1 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.xrTableCell2 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.TopMargin = New DevExpress.XtraReports.UI.TopMarginBand()
+			Me.BottomMargin = New DevExpress.XtraReports.UI.BottomMarginBand()
+			Me.DetailReport = New DevExpress.XtraReports.UI.DetailReportBand()
+			Me.Detail1 = New DevExpress.XtraReports.UI.DetailBand()
+			Me.xrTable2 = New DevExpress.XtraReports.UI.XRTable()
+			Me.xrTableRow2 = New DevExpress.XtraReports.UI.XRTableRow()
+			Me.xrTableCell4 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.xrTableCell5 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.xrTableCell6 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.xrTableCell7 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.xrTableCell8 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.ReportFooter = New DevExpress.XtraReports.UI.ReportFooterBand()
+			Me.xrLine1 = New DevExpress.XtraReports.UI.XRLine()
+			Me.sqlDataSource1 = New DevExpress.DataAccess.Sql.SqlDataSource(Me.components)
+			CType(Me.xrTable1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.xrTable2, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' Detail
+			' 
+			Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLabel1, Me.xrPictureBox1, Me.xrTable1})
+			Me.Detail.Name = "Detail"
+			Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' xrLabel1
+			' 
+			Me.xrLabel1.ExpressionBindings.AddRange(New DevExpress.XtraReports.UI.ExpressionBinding() { New DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[Description]")})
+			Me.xrLabel1.LocationFloat = New DevExpress.Utils.PointFloat(1.5625F, 25F)
+			Me.xrLabel1.Name = "xrLabel1"
+			Me.xrLabel1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrLabel1.SizeF = New System.Drawing.SizeF(404.6875F, 75.00002F)
+			Me.xrLabel1.Text = "xrLabel1"
+			' 
+			' xrPictureBox1
+			' 
+			Me.xrPictureBox1.ExpressionBindings.AddRange(New DevExpress.XtraReports.UI.ExpressionBinding() { New DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Image", "[Picture]")})
+			Me.xrPictureBox1.LocationFloat = New DevExpress.Utils.PointFloat(406.25F, 0F)
+			Me.xrPictureBox1.Name = "xrPictureBox1"
+			Me.xrPictureBox1.SizeF = New System.Drawing.SizeF(243.75F, 100F)
+			Me.xrPictureBox1.Sizing = DevExpress.XtraPrinting.ImageSizeMode.StretchImage
+			' 
+			' xrTable1
+			' 
+			Me.xrTable1.LocationFloat = New DevExpress.Utils.PointFloat(0F, 0F)
+			Me.xrTable1.Name = "xrTable1"
+			Me.xrTable1.Rows.AddRange(New DevExpress.XtraReports.UI.XRTableRow() { Me.xrTableRow1})
+			Me.xrTable1.SizeF = New System.Drawing.SizeF(406.25F, 25F)
+			' 
+			' xrTableRow1
+			' 
+			Me.xrTableRow1.Cells.AddRange(New DevExpress.XtraReports.UI.XRTableCell() { Me.xrTableCell1, Me.xrTableCell2})
+			Me.xrTableRow1.Name = "xrTableRow1"
+			Me.xrTableRow1.Weight = 11.5R
+			' 
+			' xrTableCell1
+			' 
+			Me.xrTableCell1.ExpressionBindings.AddRange(New DevExpress.XtraReports.UI.ExpressionBinding() { New DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[CategoryID]")})
+			Me.xrTableCell1.Name = "xrTableCell1"
+			Me.xrTableCell1.Text = "xrTableCell1"
+			Me.xrTableCell1.Weight = 0.4R
+			' 
+			' xrTableCell2
+			' 
+			Me.xrTableCell2.ExpressionBindings.AddRange(New DevExpress.XtraReports.UI.ExpressionBinding() { New DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[CategoryName]")})
+			Me.xrTableCell2.Name = "xrTableCell2"
+			Me.xrTableCell2.Text = "xrTableCell2"
+			Me.xrTableCell2.Weight = 1.2000000000000002R
+			' 
+			' TopMargin
+			' 
+			Me.TopMargin.Name = "TopMargin"
+			Me.TopMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.TopMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' BottomMargin
+			' 
+			Me.BottomMargin.Name = "BottomMargin"
+			Me.BottomMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.BottomMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' DetailReport
+			' 
+			Me.DetailReport.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail1, Me.ReportFooter})
+			Me.DetailReport.DataMember = "Categories.CategoriesProducts"
+			Me.DetailReport.DataSource = Me.sqlDataSource1
+			Me.DetailReport.Level = 0
+			Me.DetailReport.Name = "DetailReport"
+			' 
+			' Detail1
+			' 
+			Me.Detail1.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrTable2})
+			Me.Detail1.HeightF = 25F
+			Me.Detail1.Name = "Detail1"
+			' 
+			' xrTable2
+			' 
+			Me.xrTable2.LocationFloat = New DevExpress.Utils.PointFloat(0F, 0F)
+			Me.xrTable2.Name = "xrTable2"
+			Me.xrTable2.Rows.AddRange(New DevExpress.XtraReports.UI.XRTableRow() { Me.xrTableRow2})
+			Me.xrTable2.SizeF = New System.Drawing.SizeF(650F, 25F)
+			' 
+			' xrTableRow2
+			' 
+			Me.xrTableRow2.Cells.AddRange(New DevExpress.XtraReports.UI.XRTableCell() { Me.xrTableCell4, Me.xrTableCell5, Me.xrTableCell6, Me.xrTableCell7, Me.xrTableCell8})
+			Me.xrTableRow2.Name = "xrTableRow2"
+			Me.xrTableRow2.Weight = 11.5R
+			' 
+			' xrTableCell4
+			' 
+			Me.xrTableCell4.ExpressionBindings.AddRange(New DevExpress.XtraReports.UI.ExpressionBinding() { New DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[ProductName]")})
+			Me.xrTableCell4.Name = "xrTableCell4"
+			Me.xrTableCell4.Text = "xrTableCell4"
+			Me.xrTableCell4.Weight = 4R
+			' 
+			' xrTableCell5
+			' 
+			Me.xrTableCell5.ExpressionBindings.AddRange(New DevExpress.XtraReports.UI.ExpressionBinding() { New DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[QuantityPerUnit]")})
+			Me.xrTableCell5.Name = "xrTableCell5"
+			Me.xrTableCell5.Text = "xrTableCell5"
+			Me.xrTableCell5.Weight = 2R
+			' 
+			' xrTableCell6
+			' 
+			Me.xrTableCell6.ExpressionBindings.AddRange(New DevExpress.XtraReports.UI.ExpressionBinding() { New DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[UnitPrice]")})
+			Me.xrTableCell6.Name = "xrTableCell6"
+			Me.xrTableCell6.Text = "xrTableCell6"
+			Me.xrTableCell6.Weight = 2R
+			' 
+			' xrTableCell7
+			' 
+			Me.xrTableCell7.ExpressionBindings.AddRange(New DevExpress.XtraReports.UI.ExpressionBinding() { New DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[UnitsInStock]")})
+			Me.xrTableCell7.Name = "xrTableCell7"
+			Me.xrTableCell7.Text = "xrTableCell7"
+			Me.xrTableCell7.Weight = 2R
+			' 
+			' xrTableCell8
+			' 
+			Me.xrTableCell8.ExpressionBindings.AddRange(New DevExpress.XtraReports.UI.ExpressionBinding() { New DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[UnitsOnOrder]")})
+			Me.xrTableCell8.Name = "xrTableCell8"
+			Me.xrTableCell8.Text = "xrTableCell8"
+			Me.xrTableCell8.Weight = 2R
+			' 
+			' ReportFooter
+			' 
+			Me.ReportFooter.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLine1})
+			Me.ReportFooter.HeightF = 23F
+			Me.ReportFooter.Name = "ReportFooter"
+			' 
+			' xrLine1
+			' 
+			Me.xrLine1.LocationFloat = New DevExpress.Utils.PointFloat(0F, 0F)
+			Me.xrLine1.Name = "xrLine1"
+			Me.xrLine1.SizeF = New System.Drawing.SizeF(650F, 23F)
+			' 
+			' sqlDataSource1
+			' 
+			Me.sqlDataSource1.ConnectionName = "WindowsFormsApplication1.Properties.Settings.nwindConnectionString"
+			Me.sqlDataSource1.Name = "sqlDataSource1"
+			columnExpression1.ColumnName = "CategoryID"
+			table1.Name = "Categories"
+			columnExpression1.Table = table1
+			column1.Expression = columnExpression1
+			columnExpression2.ColumnName = "CategoryName"
+			columnExpression2.Table = table1
+			column2.Expression = columnExpression2
+			columnExpression3.ColumnName = "Description"
+			columnExpression3.Table = table1
+			column3.Expression = columnExpression3
+			columnExpression4.ColumnName = "Picture"
+			columnExpression4.Table = table1
+			column4.Expression = columnExpression4
+			columnExpression5.ColumnName = "Icon_17"
+			columnExpression5.Table = table1
+			column5.Expression = columnExpression5
+			columnExpression6.ColumnName = "Icon_25"
+			columnExpression6.Table = table1
+			column6.Expression = columnExpression6
+			selectQuery1.Columns.Add(column1)
+			selectQuery1.Columns.Add(column2)
+			selectQuery1.Columns.Add(column3)
+			selectQuery1.Columns.Add(column4)
+			selectQuery1.Columns.Add(column5)
+			selectQuery1.Columns.Add(column6)
+			selectQuery1.MetaSerializable = "<Meta X=""20"" Y=""20"" Width=""100"" Height=""139"" />"
+			selectQuery1.Name = "Categories"
+			selectQuery1.Tables.Add(table1)
+			columnExpression7.ColumnName = "ProductID"
+			table2.Name = "Products"
+			columnExpression7.Table = table2
+			column7.Expression = columnExpression7
+			columnExpression8.ColumnName = "ProductName"
+			columnExpression8.Table = table2
+			column8.Expression = columnExpression8
+			columnExpression9.ColumnName = "SupplierID"
+			columnExpression9.Table = table2
+			column9.Expression = columnExpression9
+			columnExpression10.ColumnName = "CategoryID"
+			columnExpression10.Table = table2
+			column10.Expression = columnExpression10
+			columnExpression11.ColumnName = "QuantityPerUnit"
+			columnExpression11.Table = table2
+			column11.Expression = columnExpression11
+			columnExpression12.ColumnName = "UnitPrice"
+			columnExpression12.Table = table2
+			column12.Expression = columnExpression12
+			columnExpression13.ColumnName = "UnitsInStock"
+			columnExpression13.Table = table2
+			column13.Expression = columnExpression13
+			columnExpression14.ColumnName = "UnitsOnOrder"
+			columnExpression14.Table = table2
+			column14.Expression = columnExpression14
+			columnExpression15.ColumnName = "ReorderLevel"
+			columnExpression15.Table = table2
+			column15.Expression = columnExpression15
+			columnExpression16.ColumnName = "Discontinued"
+			columnExpression16.Table = table2
+			column16.Expression = columnExpression16
+			columnExpression17.ColumnName = "EAN13"
+			columnExpression17.Table = table2
+			column17.Expression = columnExpression17
+			selectQuery2.Columns.Add(column7)
+			selectQuery2.Columns.Add(column8)
+			selectQuery2.Columns.Add(column9)
+			selectQuery2.Columns.Add(column10)
+			selectQuery2.Columns.Add(column11)
+			selectQuery2.Columns.Add(column12)
+			selectQuery2.Columns.Add(column13)
+			selectQuery2.Columns.Add(column14)
+			selectQuery2.Columns.Add(column15)
+			selectQuery2.Columns.Add(column16)
+			selectQuery2.Columns.Add(column17)
+			selectQuery2.MetaSerializable = "<Meta X=""140"" Y=""20"" Width=""100"" Height=""224"" />"
+			selectQuery2.Name = "Products"
+			selectQuery2.Tables.Add(table2)
+			Me.sqlDataSource1.Queries.AddRange(New DevExpress.DataAccess.Sql.SqlQuery() { selectQuery1, selectQuery2})
+			masterDetailInfo1.DetailQueryName = "Products"
+			relationColumnInfo1.NestedKeyColumn = "CategoryID"
+			relationColumnInfo1.ParentKeyColumn = "CategoryID"
+			masterDetailInfo1.KeyColumns.Add(relationColumnInfo1)
+			masterDetailInfo1.MasterQueryName = "Categories"
+			Me.sqlDataSource1.Relations.AddRange(New DevExpress.DataAccess.Sql.MasterDetailInfo() { masterDetailInfo1})
+			Me.sqlDataSource1.ResultSchemaSerializable = resources.GetString("sqlDataSource1.ResultSchemaSerializable")
+			' 
+			' XtraReport1
+			' 
+			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.TopMargin, Me.BottomMargin, Me.DetailReport})
+			Me.ComponentStorage.AddRange(New System.ComponentModel.IComponent() { Me.sqlDataSource1})
+			Me.DataMember = "Categories"
+			Me.DataSource = Me.sqlDataSource1
+			Me.Version = "18.1"
+			CType(Me.xrTable1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.xrTable2, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+
+		#End Region
+
+		Private Detail As DevExpress.XtraReports.UI.DetailBand
+		Private TopMargin As DevExpress.XtraReports.UI.TopMarginBand
+		Private BottomMargin As DevExpress.XtraReports.UI.BottomMarginBand
+		Private xrLabel1 As DevExpress.XtraReports.UI.XRLabel
+		Private xrPictureBox1 As DevExpress.XtraReports.UI.XRPictureBox
+		Private xrTable1 As DevExpress.XtraReports.UI.XRTable
+		Private xrTableRow1 As DevExpress.XtraReports.UI.XRTableRow
+		Private xrTableCell1 As DevExpress.XtraReports.UI.XRTableCell
+		Private xrTableCell2 As DevExpress.XtraReports.UI.XRTableCell
+		Private DetailReport As DevExpress.XtraReports.UI.DetailReportBand
+		Private Detail1 As DevExpress.XtraReports.UI.DetailBand
+		Private ReportFooter As DevExpress.XtraReports.UI.ReportFooterBand
+		Private xrLine1 As DevExpress.XtraReports.UI.XRLine
+		Private xrTable2 As DevExpress.XtraReports.UI.XRTable
+		Private xrTableRow2 As DevExpress.XtraReports.UI.XRTableRow
+		Private xrTableCell4 As DevExpress.XtraReports.UI.XRTableCell
+		Private xrTableCell5 As DevExpress.XtraReports.UI.XRTableCell
+		Private xrTableCell6 As DevExpress.XtraReports.UI.XRTableCell
+		Private xrTableCell7 As DevExpress.XtraReports.UI.XRTableCell
+		Private xrTableCell8 As DevExpress.XtraReports.UI.XRTableCell
+		Private sqlDataSource1 As DevExpress.DataAccess.Sql.SqlDataSource
+	End Class
+End Namespace
